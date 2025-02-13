4034fa013278742a9e877ffc1cf9f441fda3a11b
mutai001 <omusekim@gmail.com>
2025-02-13 16:48:00 +0300

    pushed schema

--- src/stripe/stripe.ts
+++ src/stripe/stripe.ts
@@ -1,5 +1,10 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+if (!stripeSecretKey) {
+  throw new Error('Missing Stripe Secret Key. Please set the STRIPE_SECRET_KEY environment variable.');
+}
+
+export const stripe = new Stripe(stripeSecretKey, {
   apiVersion: '2025-01-27.acacia',
 });
