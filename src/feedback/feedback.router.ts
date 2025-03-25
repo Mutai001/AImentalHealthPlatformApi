74629f05290811e0e5105a2f124e886eb4ef210f
Mutai001 <omusekim@gmail.com>
2025-03-25 22:24:36 +0300

    feedback

--- src/feedback/feedback.router.ts
+++ src/feedback/feedback.router.ts
@@ -2,13 +2,17 @@ import { Hono } from "hono";
 import { listfeedbacks,getfeedbacks , createfeedbacks, updatefeedbacks, deletefeedbacks } from "./feedback.controller"
 import { zValidator } from "@hono/zod-validator";
 import { feedbackSchema} from "./validator"; 
-import { adminRoleAuth } from '../middleware/bearAuth'
+// import { adminRoleAuth } from '../middleware/bearAuth'
 export const feedbackRouter = new Hono();
 //get all feedbacks
-feedbackRouter.get("/feedbacks",adminRoleAuth ,listfeedbacks) 
+feedbackRouter.get("/feedbacks",
+    // adminRoleAuth ,
+    listfeedbacks) 
 
 //get a single therapist   api/therapist/1
-feedbackRouter.get("/feedbacks/:id",adminRoleAuth, getfeedbacks)
+feedbackRouter.get("/feedbacks/:id"
+    // ,adminRoleAuth
+    , getfeedbacks)
 
 // create a therapist 
 feedbackRouter.post("/feedbacks", zValidator('json', feedbackSchema, (result, c) => {
